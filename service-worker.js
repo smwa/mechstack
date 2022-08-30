1ab7784397071d7616f529ed85248c3ea6affeb8
Michael Smith <michael.smith.ok@gmail.com>
2022-08-30 16:38:53 -0500

    Add all assets, lib, and html pages to service worker

--- service-worker.js
+++ service-worker.js
@@ -16,16 +16,19 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-{{ site.time }}';
+const PRECACHE = "precache-{{ site.time | replace:' ','' | replace:':','' | replace:'-','' }}";
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'assets/css/custom.css',
-  'lib/feather.min.js',
-  'lib/skeleton/skeleton.css'
+  {% assign assets = site.static_files | where: "asset", true %}
+  {% for asset in assets %}
+    '.{{ asset.path }}',
+  {% endfor %}
+  {% for page in site.html_pages %}
+    '.{{ page.url }}',
+  {% endfor %}
+  './index.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
